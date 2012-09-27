ffc95647f7916d21cd0a6d5951521dcf7ee5d7e4
mystelynx <tomohiro.urakawa@gmail.com>
2012-09-27 18:01:09 +0900

    session store

--- src/main/scala/com/github/mystelynx/server.scala
+++ src/main/scala/com/github/mystelynx/server.scala
@@ -20,17 +20,20 @@ class Nekopic extends Plan with ThreadPool with ServerErrorResponse {
   val log = Logger(classOf[Nekopic])
 
   def intent = {
-    case req @ GET(Path("/")) => Ok ~> Html { <a href="/oauth/connect">Connect with Instagram</a>}
+    case req @ GET(Path("/")) => {
+      val sid = SimpleSessionStore.createSession
+      Ok ~> SetCookies(Cookie("sid", sid)) ~>Html { <a href="/oauth/connect">Connect with Instagram</a>}
+    }
     case GET(Path("/oauth/connect")) => Redirect(Instagram.authorizeUrl(CALLBACK_URL))
-    case req @ GET(Path("/oauth/callback")) & Params(InstagramAuthSuccess(code)) => {
+    case req @ GET(Path("/oauth/callback")) & Params(InstagramAuthSuccess(code)) & Cookies(cs) => {
       val resp = Instagram.getAccessToken(code)(CALLBACK_URL)
-      println(resp.toString)
-      SetCookies(Cookie("ins", resp.access_token)) ~> Redirect("/feed")
+      val sid = cs("sid").map(_.value).getOrElse("unknown session id")
+      SimpleSessionStore.setSessionAttribute(sid, "ins", resp.access_token)
+      Redirect("/feed")
     }
     case req @ GET(Path("/feed")) & Cookies(cs) => {
-      println("authorized. get feed...")
-      println(cs.mkString)
-      val token = cs("ins").map(_.value).getOrElse("???")
+      val sid = cs("sid").map(_.value).getOrElse("unknown session id")
+      val token = SimpleSessionStore.getSessionAttribute(sid, "ins").map(_.toString).getOrElse("unkonwn_access_token")
       val client = new Instagram.Client(token)
       val resp = client.users_self_feed
 
@@ -45,3 +48,33 @@ object InstagramAuthToken {
   val REDIRECT_URI = "http://nekopic.herokuapp.com/redirect"
 }
 object InstagramAuthSuccess extends Params.Extract("code", Params.first ~> Params.nonempty ~> Params.trimmed)
+
+object SimpleSessionStore {
+  import scala.collection._
+  private val store = mutable.Map[String, mutable.Map[String, Any]]()
+
+  def createSession = {
+    val id = generate
+    SimpleSessionStore.synchronized {
+      store += (id->mutable.Map[String, Any]())
+    }
+    id
+  }
+
+  def getSession(id: String) = store get id
+  def removeSession(id: String) = store remove id
+  def setSessionAttribute(id: String, key: String, value: Any) = store get id match {
+    case Some(map) => map += (key->value)
+    case _ => throw new IllegalStateException("session store not created")
+  }
+  def getSessionAttribute(id: String, key: String) = store get id match {
+    case Some(map) => map get id
+    case _ => throw new IllegalStateException("session store not created")
+  }
+  def removeSessionAttribute(id: String, key: String) = store get id match {
+    case Some(map) => map remove id
+    case _ => throw new IllegalStateException("session store not created")
+  }
+
+  protected def generate = scala.util.Random.alphanumeric.take(256).mkString
+}
